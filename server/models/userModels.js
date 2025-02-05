cc6a31ffcd083f35bef4dd99deac42ac80190773
Gauransh Kumar <106055075+GAURANSH-26@users.noreply.github.com>
2025-02-05 21:45:58 +0530

    list_databases

--- server/models/userModels.js
+++ server/models/userModels.js
@@ -1,5 +1,29 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    
-})
+   name:{
+    type:String,
+    required:true
+   },
+
+   email:{
+    type:String,
+    required:true
+   },
+   
+   password:{
+    type:String,
+    required:true
+   },
+   avatar:{
+    type:String
+   },
+   posts:{
+    type:Number,
+    default:0
+   }
+});
+
+const userModel = mongoose.model('users', userSchema)
+
+module.exports = {userModel }
